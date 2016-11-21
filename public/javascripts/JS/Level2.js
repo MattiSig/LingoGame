71408d90a4342c2fd1f736613c2894e34b3dfd7a
arnorpetur <apm2@hi.is>
2016-11-21 17:29:42 +0000

    loksins allt

--- public/javascripts/JS/Level2.js
+++ public/javascripts/JS/Level2.js
@@ -2,19 +2,20 @@
 //creates "private" variables to be called privately within 
 //game.prototype object
 Lingo.Level2 = function(game) {
-	this.map;
-	this.layer;
-	this.layer2;
+    this.map;
+    this.layer;
+    this.layer2;
     this.layer3;
-	this.p;
-	this.cursors;
-	this.jumpButton;
-	this.jumpTimer = 0;
-	this.player;
+    this.p;
+    this.cursors;
+    this.jumpButton;
+    this.jumpTimer = 0;
+    this.player;
     this.playerFacing = 'right';
-	
+    this.lifeTimer= 0;
+    
     this.enemy;
-	this.text1;
+    this.text1;
     this.text2;
     this.game = game;
     this.button;
@@ -45,11 +46,11 @@ var isWithinRange;
 //8===========================D
 
 Lingo.Level2.prototype = {
-    //create level2 objects to be renderd on screen
-	create: function () {
+    //create game objects to be renderd on screen
+    create: function () {
 
     this.time.advancedTiming = true;
-	
+    
     this.stage.backgroundColor = '#989898';
     
     //------------------------
@@ -64,46 +65,40 @@ Lingo.Level2.prototype = {
 
     //this.layer.debug = true;
     this.map.setCollision([1,2,3,4,5,8,9,10,11,12,15,16,17],true,'base');
-
+    this.map.setCollision([36,37,38,39,40],true,'bgMountains');
     //-------Items(scripts)--------
     this.script = this.add.group();
     this.script.enableBody = true;
 
-    this.map.createFromObjects('scriptSpawn', 27, 'script', 0, true, false, this.script);
+    this.map.createFromObjects('objectsSpawn', 27, 'script', 0, true, false, this.script);
     //------------------------------
     //-----------player-------------
-    this.player = this.add.sprite(250, 2800, 'rundog');
-    //this.player.anchor.setTo(.5,.5);
-    this.physics.enable(this.player, Phaser.Physics.ARCADE);
-    this.player.enableBody = true;
-    //this.player.physicsBodyType = Phaser.Physics.ARCADE;
-
-    this.player.body.tilePadding.set(32);
-    this.player.body.collideWorldBounds = true;
-    this.player.body.gravity.y = 700;
-    this.player.body.setSize(70, 50, 4, 0);
+    this.player = new Lingo.Player(this.game,250, 2800);
+    this.add.existing(this.player);
+    this.add.existing(this.player.tounges);
     this.camera.follow(this.player);
 
-    this.player.animations.add('right',[0,1,2,3,4,5,6,7], 12, true);
-    this.player.animations.add('left',[15,14,13,12,11,10,9,8], 12, true);
-    this.player.animations.add('sit',[16,17,18,19,20], 12, true);
     //------------------------------
     //-----------Enemy--------------
-    this.enemy = this.add.sprite(2325, 2700, 'ms');
-    this.enemy.animations.add('walk');
-    this.enemy.animations.play('walk', 50, true);
+    this.enemy = this.add.group();
+    this.enemy.enableBody = true;
+    this.map.createFromObjects('objectsSpawn', 35, 'ms', 0, true, false, this.enemy);
+
+    this.enemy.callAll('animations.add', 'animations', 'walk', [0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18], 17, true);
+    this.enemy.callAll('animations.play', 'animations', 'walk');
 
     this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
-    this.enemy.enableBody = true;
-    //this.enemy.physicsBodyType = Phaser.Physics.ARCADE;
-    //this.enemy.body.setSize(4, 4, 14, 26);
-    this.enemy.body.tilePadding.set(32);
-    this.enemy.body.collideWorldBounds = true;
-    this.enemy.body.velocity.x = 50;
-    this.enemy.body.bounce.x = 1;
-    this.enemy.body.gravity.y = 700;
-    this.enemy.anchor.setTo(.5,.5);
-    this.enemy.scale.setTo(-1,1);
+    console.log(this.enemy.children[0]);
+
+    for (var i = 0; i < this.enemy.children.length; i++)
+        {
+            var aEnemy = this.enemy.children[i];
+            aEnemy.body.velocity.x = 50;
+            aEnemy.body.bounce.x = 1;
+            aEnemy.body.gravity.y = 100;
+            aEnemy.anchor.setTo(.5,.5);
+        }
+
     //8==========================================D
     $.ajax({
         type: 'GET',
@@ -155,7 +150,7 @@ Lingo.Level2.prototype = {
 
     //------------------------------
     //-----------text-------------
-    this.text1 = this.add.text(this.enemy.body.x, this.enemy.body.y, dictionary[0].enska, { font: "14px Arial Black", fill: "#FFFFFF" });
+    this.text1 = this.add.text(this.enemy.children[0].body.x, this.enemy.children[0].body.y, dictionary[0].enska, { font: "14px Arial Black", fill: "#FFFFFF" });
     this.text1.anchor.setTo(0.5, 0.5);
 
     this.text2 = this.add.text(700, 2700,dictionary[textNumber].enska+" = "+dictionary[textNumber].islenska,{ font: "18px Arial Black", fill: "#FFFFFF" });
@@ -173,40 +168,32 @@ Lingo.Level2.prototype = {
     this.fButton = this.input.keyboard.addKey(Phaser.Keyboard.F);
     //8===============================================================D
     console.log(this.pauseButton);
-	},
+    },
     //game update loop
-	update: function() {
+    update: function() {
 
         this.deltaTime = this.time.elapsedMS / 1000;
-		
-        this.player.body.velocity.x = 0;
+        
+        //this.player.body.velocity.x = 0; -----------------------------------
         //stick text to enemy
-        this.text1.x = this.enemy.body.x ;
-        this.text1.y = this.enemy.body.y - 40;
+        this.text1.x = this.enemy.children[0].body.x ;
+        this.text1.y = this.enemy.children[0].body.y - 40;
         //handle controler events
         if (this.cursors.left.isDown){
-            this.player.body.x = this.player.body.x + (-300 * this.deltaTime);
-            this.player.animations.play('left');
-            this.playerFacing = 'left';
-
+            this.player.moveLeft(this.deltaTime);
         }else if (this.cursors.right.isDown){
-            this.player.body.x = this.player.body.x + 300 * this.deltaTime;
-            this.player.animations.play('right');
-            this.playerFacing = 'right';
-        
+            this.player.moveRight(this.deltaTime);
         }else{
-            if (this.playerFacing != 'idle'){
+            if (this.player.playerFacing != 'idle'){
                 this.player.animations.stop();
-                if (this.playerFacing == 'left'){
+                if (this.player.playerFacing == 'left'){
                     this.player.frame = 21;
                 }else{
                     this.player.frame = 16;
-                    
                 }
-                this.playerFacing = 'idle';
+                this.player.playerFacing = 'idle';
             }
-        }
-    
+        }    
         
         this.physics.arcade.collide(this.player, this.layer);
 
@@ -221,41 +208,43 @@ Lingo.Level2.prototype = {
         }
 
         isWithinRange = (
-            (this.player.body.x > this.enemy.body.x - 400) && 
-            (this.player.body.x < this.enemy.body.x + 200) && 
-            (this.player.body.y < this.enemy.body.y +500) && 
-            (this.player.body.y > this.enemy.body.y - 500)
+            (this.player.body.x > this.enemy.children[0].body.x - 400) && 
+            (this.player.body.x < this.enemy.children[0].body.x + 200) && 
+            (this.player.body.y < this.enemy.children[0].body.y +500) && 
+            (this.player.body.y > this.enemy.children[0].body.y - 500)
         )
         if(isWithinRange && (this.button1.alpha === 0)){
             this.makeVisible(true);
-            console.log('true')
         }else if(!isWithinRange && (this.button1.alpha === 1)) {
             this.makeVisible(false);
-            console.log('false');
         }
         
         this.physics.arcade.collide(this.enemy, this.layer);
-        this.physics.arcade.collide(this.enemy,this.player,function(player, enemy) {
-            this.player.kill();
-            this.physics.arcade.isPaused = (this.physics.arcade.isPaused) ? false : true;
+        this.physics.arcade.collide(this.enemy,this.player, function(player, enemy) {
+            this.player.looseLife();
+            console.log("bluh");
         },null, this);
         this.physics.arcade.overlap(this.player, this.script, this.collectScript, null, this);
-
+        this.physics.arcade.collide(this.player, this.layer3, function(player, layer3){
+            console.log(this.time.now)
+            this.player.looseLife(this.time.now);
+        },null, this);
+        
         this.pauseButton.onDown.add(this.pause, this);        
         //8====================================D
-        this.aButton.onDown.add(this.isCorrect, this.button1);
-        this.sButton.onDown.add(this.isCorrect, this.button2);
-        this.dButton.onDown.add(this.isCorrect, this.button3);
-        this.fButton.onDown.add(this.isCorrect, this.button4);
+        this.aButton.onDown.add(this.isCorrect, this, 0, this.button1, this.enemy.children[0]);
+        this.sButton.onDown.add(this.isCorrect, this, 0, this.button2, this.enemy.children[0]);
+        this.dButton.onDown.add(this.isCorrect, this, 0, this.button3, this.enemy.children[0]);
+        this.fButton.onDown.add(this.isCorrect, this, 0, this.button4, this.enemy.children[0]);
 
-        /*if (this.enemy.body.blocked.left === true || this.enemy.body.blocked.right === true) {
-
-        }*/
-	},
+        for (var i = 0; i < this.enemy.children.length; i++){
+            if(this.enemy.children[i].body.blocked.left === true || this.enemy.children[i].body.blocked.right === true){
+                this.enemy.children[i].scale.x *= -1;
+            }
+        }
+    },
     //render debug text
     render: function(){
-        this.game.debug.bodyInfo(this.player, 32, 32);
-        //this.game.debug.body(this.player);
         this.game.debug.text("FPS: " + this.game.time.fps, 2, 14,"#00ff00");
         this.game.debug.text("ms.time: " +  this.deltaTime, 2, 42, "#00ff00" );
         this.game.debug.text(this.game.time.now, 2, 70, "#00ff00");
@@ -263,7 +252,6 @@ Lingo.Level2.prototype = {
     },
     pause: function(){
         this.physics.arcade.isPaused = (this.physics.arcade.isPaused) ? false : true;
-
     },
     makeVisible: function(isHidden){
         if(isHidden){
@@ -279,18 +267,21 @@ Lingo.Level2.prototype = {
             this.button4.alpha = 0;
         }
     },
-    isCorrect: function(){
-        console.log(this);
-        var buttonText = this.buttonText._text;
+    isCorrect: function(arg1, arg2, arg3){
+        // console.log(this);
+        // console.log(arg2);
+        // console.log(arg3);
+        var buttonText = arg2.buttonText._text;
         if(buttonText===dictionary[0].islenska){
+            arg3.kill();
             console.log('réttur takki');
-        } else{
+        } else {
             console.log('vitlaus takki');
         }
     },
     collectScript: function(player, script) {
         script.destroy();
-        this.text2 = this.add.text(700, 2700, 'level 2 stuff',{ font: "18px Arial Black", fill: "#FFFFFF" });
+        this.text2 = this.add.text(700, 2700, 'level 2 bitch',{ font: "18px Arial Black", fill: "#FFFFFF" });
         this.tempScriptTime = this.time.now + 3000;
         this.text2.x = script.x -100;
         this.text2.y = script.y -200;
